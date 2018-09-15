17774627354f672ff323171337848dd92965f412
Usuario Salón <salon@academico.ucu.edu.uy>
2018-09-15 12:52:38 -0300

    Code example django

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .models import Question
 
-# Create your views here.
+@csrf_exempt
+def traeme(request):
+    if request.method == "GET":
+        questions = list(Question.objects.filter(question_text="En que anda la banda ?"))
+        response = list(map(lambda x : str(x), questions))
+        return JsonResponse({"questions": response})
+    if request.method == "POST":
+        Question.objects.create(
+            question_text = request.POST["question_text"],
+            pub_date = request.POST["date"]
+        )
+        return JsonResponse({"status":"success"})
