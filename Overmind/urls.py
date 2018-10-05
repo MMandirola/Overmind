e42e822dff4eafdc807f3314b1c4d80234eae352
iNVE14 <lucasastor@gmail.com>
2018-10-05 19:12:06 -0300

    Function using Pymongo to store rules in MongoDB

--- Overmind/urls.py
+++ Overmind/urls.py
@@ -2,10 +2,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 from api.views import ReplaysResource
+from api.views import store_rules
 
 replays_resources = ReplaysResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include(replays_resources.urls)),
+    url(r'^rule/', store_rules)
 ]
