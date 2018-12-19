baa1b8b2f622edf7ea1a656e244a64aae2f51f47
marcelo-oktana <marcelom@oktana.io>
2018-12-19 00:26:03 -0300

    add validations

--- api/views.py
+++ api/views.py
@@ -16,26 +16,41 @@ def replays(request):
         if(oponent):
             replays = Replays.objects.filter(
                 processed=False, player=player, oponent=oponent)[:1000]
-            return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            if replays:
+                return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            else:
+                return HttpResponseNotFound("No replays")
         else:
             replays = Replays.objects.filter(
                 processed=False, player=player)[:1000]
-            return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            if replays:
+                return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            else:
+                return HttpResponseNotFound("No replays")
     else:
         if(oponent):
             replays = Replays.objects.filter(
                 processed=False, oponent=oponent)[:1000]
-            return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            if replays:
+                return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            else:
+                return HttpResponseNotFound("No replays")
         else:
             replays = Replays.objects.filter(
                 processed=False)[:1000]
-            return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            if(replays):
+                return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+            else:
+                return HttpResponseNotFound("No replays")
 
 
 def replays_classify(request):
     replays = Replays.objects.filter(
         processed=False, player="", oponent="")[:1000]
-    return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+    if replays:
+        return JsonResponse(replays[random.randint(0, len(replays) - 1)].toDict())
+    else:
+        return HttpResponseNotFound("No replays")
 
 
 @csrf_exempt
@@ -56,3 +71,23 @@ def classify(request):
             replay.save()
             return HttpResponse()
     return HttpResponseNotFound()
+
+# @csrf_exempt
+# def process(request):
+#     if request.method == "POST":
+#         id = request.POST.get("id")
+#         player = request.POST.get("player")
+#         opponent = request.POST.get("opponent")
+#         map = request.POST.get("map")
+
+#         replays = Replays.objects.filter(
+#             processed=False, title=id, player="", oponent="")
+#         if replays:
+#             replay = replays[0]
+#             replay.player = player
+#             replay.oponent = opponent
+#             replay.map = map
+#             replay.save()
+#             return HttpResponse()
+#     return HttpResponseNotFound()
+
