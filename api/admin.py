89c91a35929add3db0ec5d2ac89d3e3d09dda28f
Marcelo <marcelosiccardi@gmail.com>
2019-02-22 22:02:54 -0300

    Add replay download action

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,40 @@
+import os
+import shutil
+import base64
+
+from wsgiref.util import FileWrapper
+
+from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
+
 from api.models import Replays, Mode, Feedback, Stat
+
+
+def download_replays(modeladmin, request, queryset):
+    filenames = []
+    temp_folder = settings.BASE_DIR + "/tmp"
+    if not os.path.isdir(temp_folder):
+        os.makedirs(temp_folder)
+    for replay in queryset.all():
+        file_name = "{}/{}".format(temp_folder, replay.title.split("/")[-1])
+        filenames.append(file_name)
+        replay_file = open(file_name, "wb")
+        replay_file.write(base64.b64decode(replay.base64_file))
+        replay_file.close()
+
+    file_name = "SC2Replays"
+    files_path = temp_folder + "/"
+    path_to_zip = shutil.make_archive(files_path, "zip", files_path)
+    response = HttpResponse(FileWrapper(open(path_to_zip, 'rb')), content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename="{filename}.zip"'.format(
+        filename=file_name.replace(" ", "_")
+    )
+    shutil.rmtree(temp_folder)
+    return response
+
+
+download_replays.short_description = "Download replays"
 
 
 class ReplayAdmin(admin.ModelAdmin):
@@ -17,6 +52,7 @@ class StatAdmin(admin.ModelAdmin):
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('title','processed')
     list_filter = ('processed',)
+    actions = [download_replays]
 
 admin.site.register(Replays, ReplayAdmin)
 admin.site.register(Mode, ModeAdmin)
