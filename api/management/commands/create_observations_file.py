7469d2950054a8b70abe519418bb2126755319cc
Marcelo <marcelosiccardi@gmail.com>
2019-02-24 19:13:03 -0300

    Update observation creation command

--- api/management/commands/create_observations_file.py
+++ api/management/commands/create_observations_file.py
@@ -12,21 +12,38 @@ import time
 from bson.json_util import dumps
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--group',
+            dest='grouped',
+            help='Generate file with grouped observations only',
+        )
+        parser.add_argument(
+            '--filename',
+            dest='filename',
+            help='Specify file name',
+        )
 
     def handle(self, *args, **options):
+        file_name = options.get('filename') if options.get('filename') is not None else 'observations'
         client = MongoClient()
         mongo_db = client.sc2
         db_observations = mongo_db.observations
         observations = []
-        obs_file = open("static/observations.json", "w")
+        obs_file = open("static/{}.json".format(file_name), "w")
         for i in range(0,180):
             print(i)
             time.sleep(0.5)
+            if options.get('grouped'):
+                match_query = {"$match": {"observation.loop": i * 240, "games": {"$gte": 2}}}
+            else:
+                match_query = {"$match": {"observation.loop": i * 240}}
             pipeline = [
-                {"$match": {"observation.loop": i * 240}},
+                match_query,
                 {"$sort": {"observation.games": -1}},
                 {"$limit": 200}
             ]
             observations += db_observations.aggregate(pipeline, allowDiskUse=True)
+        print("Writing to file {}.json".format(file_name))
         obs_file.write(dumps(observations))
-        
+        obs_file.close()
